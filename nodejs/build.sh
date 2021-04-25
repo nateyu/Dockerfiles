1ec3736d1eb09306d6588c417ec76e3f71764062
yudiegui <yudiegui@yudieguideMacBook-Pro-M1.local>
2021-04-25 22:43:47 +0800

    add build.sh

---
+++ nodejs/build.sh
@@ -0,0 +1,4 @@
+# /bin/env bash
+
+wget Dockerfile http://git.mchz.com.cn/base-platform/Dockerfiles/raw/master/nodejs/Dockerfile
+wget http://git.mchz.com.cn/base-platform/Dockerfiles/raw/master/nodejs/.dockerignore
